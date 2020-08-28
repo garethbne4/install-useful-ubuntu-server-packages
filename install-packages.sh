337a55f0250c255ff94eaf78dce29ca3f3a2549d
Gareth <63631862+garethbne4@users.noreply.github.com>
2020-08-29 07:22:09 +1000

    initial commit (draft)

---
+++ install-packages.sh
@@ -0,0 +1,110 @@
+#!/bin/sh 
+
+apt-get update 
+
+apt-get install synaptic gparted pv mosh htop desktop-base desktop-profiles devscripts dialog eject 
+
+apt-get install dictionaries-common enchant2 diffutils distro-info enscript 
+
+apt-get install network-manager network-manager-config-connectivity-ubuntu connman connman-ui connman-gtk connman-vpn connman-doc
+
+apt-get install pekwm pekwm-themes hpanel
+
+apt-get install xfwm4 xfce4 xfdesktop4 wbar 
+
+apt-get install lightdm lxqt lxqt-config-l10n 
+
+apt-get install xfce4-theme-breeze greybird-gtk-theme bluebird-gtk-theme qt5-style-qvantum qt5-style-qvantum-l10n qt5-style-qvantum-themes 
+
+apt-get install adwaita-qt adwaita-icon-theme  
+
+apt-get install caja caja-admin caja-sendto caja-dropbox mousepad cheese cheese-common 
+
+apt-get install # aewm++-goodies 
+
+apt-get install chromium cog # qutebrowser epiphany-browser epiphany-browser-data ?? 
+
+apt-get install # akqml # http://webcamoid.github.io/ 
+
+apt-get install # aspnetcore azure-cli code? c# ??
+
+apt-get install audacity audacity-data 
+
+apt-get install backup-manager backup-manager-doc
+
+apt-get install bacula-common bacula-server bacula-tray-manager bacula-common-mysql bacula-console bacula-console-qt bacula-director bacula-doc 
+
+apt-get install barman 
+
+apt-get install beancount python3-bottle python3-bs4 python3-googleapi python3-requests
+
+apt-get install # homebank eqonomize ??
+
+apt-get install bluefish bluefish-data bluefish-plugins 
+
+apt-get install bluetooth bluez-tools 
+
+apt-get install build-essential crossbuild-essential-arm64 crossbuild-essential-armhf crossbuild-essential-armel crossbuild-essential-amd64
+
+apt-get install bzip2 bzip2-doc 
+
+apt-get install ca-cacert ca-certificates-mono 
+
+apt-get install openssh
+
+apt-get install wireguard 
+
+apt-get install cifs-utils 
+
+apt-get install citadel-suite 
+
+apt-get install claws-mail claws-mail-i18n claws-mail-plugins claws-mail-pgp-mime claws-mail-feeds-reader claws-mail-doc 
+
+apt-get install cmake cmake-data cmake-qt-gui cmake-doc 
+
+apt-get install codeblocks codebocks-common codeblocks-contrib 
+
+apt-get install cups #
+
+apt-get install dbus 
+
+apt-get install # debconf 
+
+apt-get install doc-base doc-debian debian-refcard debian-reference developers-reference
+
+apt-get install debianutils # debmake debram debram-data 
+
+apt-get install # deepin 
+
+apt-get install deja-dup deja-dup-caja
+
+apt-get install # texlive diploma 
+
+apt-get install # docker.io docker-registry docker-doc 
+
+apt-get install # nodejs npm yarn
+
+apt-get install # php7 laravel?? 
+
+apt-get install # git gitea gitserver?? 
+
+apt-get install eslint coffeescript # typescript?? 
+
+apt-get install zsh
+
+apt-get install evilwm
+
+apt-get install exfat-fuse exfat-utils 
+
+apt-get install # expect tk tcl ??
+
+apt-get install
+
+apt-get install
+
+
+
+
+
+
+apt-get upgrade 
